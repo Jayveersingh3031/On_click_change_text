5571155b6c4e21111f1815ebf1d17db9ac3508a3
Jayveer Singh Pawar <98515576+Jayveersingh3031@users.noreply.github.com>
2026-08-25 17:41:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const arr =["apple", "mango,","grapes","banana"]
+let i=0;
+let h=document.querySelector("#h");
+h.addEventListener("click",function(){
+    h.textContent=arr[i]
+    i++;
+    if(i==4){
+        i=0
+    }
+})
